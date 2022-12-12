23533aac08f26cb93bdccae89a9957b623b4acd6
* <oyugibruce2017@gmail.com>
2022-12-12 14:07:59 +0300

    Sorting-Visualiz

---
+++ precache-manifest.8b162304233d2ca0f148cd54c7d65058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954d063dc6a32588cef0a24453d43ff3",
+    "url": "/AlgoSort-Sorting-Visualiser/index.html"
+  },
+  {
+    "revision": "a17a2a290e2c7a685e7c",
+    "url": "/AlgoSort-Sorting-Visualiser/static/css/main.f2f50f5c.chunk.css"
+  },
+  {
+    "revision": "562f9cca6ba643069ef6",
+    "url": "/AlgoSort-Sorting-Visualiser/static/js/2.4db1bba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AlgoSort-Sorting-Visualiser/static/js/2.4db1bba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17a2a290e2c7a685e7c",
+    "url": "/AlgoSort-Sorting-Visualiser/static/js/main.219eadff.chunk.js"
+  },
+  {
+    "revision": "d8d4128aa9067e942531",
+    "url": "/AlgoSort-Sorting-Visualiser/static/js/runtime-main.5f4ef3ee.js"
+  }
+]);
